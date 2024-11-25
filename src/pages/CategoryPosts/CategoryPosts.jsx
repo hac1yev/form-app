99027fbc5bcd4462c38a362ef4bfb726a478fe0a
jabbarli <elvincabbarl@fmail.com>
2024-11-25 23:42:16 +0200

    Like temasi

--- src/pages/CategoryPosts/CategoryPosts.jsx
+++ src/pages/CategoryPosts/CategoryPosts.jsx
@@ -1,14 +1,16 @@
-import { useSearchParams } from 'react-router-dom';
-import Posts from '../../components/Home/Posts';
-import '../Home/Home.scss';
+import {
+  useLocation,
+  //  useSearchParams
+} from "react-router-dom";
+import Posts from "../../components/Home/Posts";
+import "../Home/Home.scss";
 
 const CategoryPosts = () => {
-    const [searchParams] = useSearchParams();
-    const categoryId = searchParams.get("category_id");    
-
-    return (
-        <Posts endpoint={`post/category/${categoryId}`} />
-    );
+  // const [searchParams] = useSearchParams();
+  // const categoryId = searchParams.get("category_id");
+  const { pathname } = useLocation();
+  const categoryId = pathname.at(-1);
+  return <Posts endpoint={`post/category/${categoryId}`} />;
 };
 
 export default CategoryPosts;
